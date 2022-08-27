ddf5ecc6495590ee93e0c303f04b511f5ef2533a
DanielAy0 <smartspice63@gmail.com>
2022-08-27 03:55:27 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('.menu')
+const close = document.querySelector('.close')
+const nav = document.querySelector('nav')
+
+menu.addEventListener('click', () => {
+    nav.classList.add('open-nav') 
+})
